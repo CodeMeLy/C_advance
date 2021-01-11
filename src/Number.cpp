9876fff002824f11fc79be57881a3b407b2edefc
Dungpi <phamdung.080602@gmail.com>
2021-01-11 12:38:43 +0700

    thêm hàm hoán vị truyền vào tham chiếu và truyền vào con trỏ

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,9 +1,16 @@
 #include<stdio.h>
 void enter(int *first, int *second);// su dung con tro de nhap
+void enter(int &first, int &second);
+void swap(int *first, int *second);
+void swap(int &first, int &second);
 void print(int first, int second);// su dung tham tri de xuat
 int main(){
     int first, second;
-    enter(&first, &second);
+    enter(&first, &second);// gọi hàm truyền vào con trỏ
+    // enter(first,second);// gọi hàm truyền vào tham chiếu
+    print(first,second);
+    swap(&first, &second);
+    printf("\nafter swap: ");
     print(first,second);
     return 0;
 }
@@ -12,6 +19,22 @@ void enter(int *first, int *second){// con tro chua dia chi
     scanf("%d",first);// scanf truyen vao 1 dia chi
     printf("enter second number:");
     scanf("%d",second);
+}
+void enter(int &first, int &second){// truyền vào tham chiếu
+    printf("enter first number: ");
+    scanf("%d",&first);// scanf truyen vao 1 dia chi, &first: lập địa chỉ của first
+    printf("enter second number:");
+    scanf("%d",&second);
+}
+void swap(int *first, int *second){
+    int temp = *first;// *first: lấy giá trị của con trỏ first
+    *first = *second;
+    *second = temp;
+}
+void swap(int &first, int &second){
+     int temp = first;
+    first = second;
+    second = temp;
 }
 void print(int first, int second){
     printf("\n{first number:%d, second number: %d}",first,second);
