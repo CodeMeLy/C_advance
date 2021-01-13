d9e9dfeb5b32a323529c1f4d65c92e55ffa7e197
thaitam <phithaitam@gmail.com>
2021-01-13 20:15:57 +0700

    nếu không đọc được file do file không tồn tại thì tạo mới file

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include <sys/stat.h>
+bool existFile(char*path);
+void createDefaultInputFile(FILE *file, char *path,const int first,const int second);
 void import(FILE *file, char *path, int &first, int &second);
 void exportMaxValue(FILE *file, char *path,const int first,const int second);
 void exportCaculatorResult(FILE *file, char *path,const int first,const int second);
@@ -17,7 +20,7 @@ int main(){
     char* input_path = "./data/data.in";
     char* max_path = "./data/max.out";
     char* caculator_path = "./data/caculator.out";
-    int first, second;
+    int first = 0, second = 0;
     import(file,input_path,first,second);
     exportMaxValue(file,max_path,first,second);
     exportCaculatorResult(file,caculator_path,first,second);
@@ -31,11 +34,20 @@ int main(){
 
     return 0;
 }
+void createDefaultInputFile(FILE *file, char *path,const int first,const int second){
+    const char* mode = "w";
+    if(!existFile(path)){// nếu chưa tồn tại file thì mới tạo mới file
+        file = fopen(path,mode);
+        fprintf(file,"%d %d\n",first,second);
+        fclose(file);
+    }
+}
 void import(FILE *file,char *path, int &first, int &second){
+
     const char* mode = "r";
     file = fopen(path,"r");
-    if(file == NULL){
-        printf("không thể mở file!");
+    if(file == NULL){// không có file thì tạo mới file
+        createDefaultInputFile(file,path,first,second);
     }
     else{
         fscanf(file,"%d",&first);
@@ -83,27 +95,23 @@ int findMax(const int first, const int second){
 void print(const int first,const int second){
     printf("\n{first number:%d, second number: %d}",first,second);
 }
+bool existFile(char*path){
+    struct stat buffer;
+    bool exist = !stat(path, &buffer);
+    return exist;
+}
+
 void exportMaxValue(FILE *file, char *path,const int first,const int second){
     const char* mode = "w";
     const int max = findMax(first,second);
     file = fopen(path,mode);
-    if(file == NULL){
-        printf("file không mở được!");
-    }
-    else{
         fprintf(file,"first: %d, second:%d\n",first,second);
         fprintf(file,"max = %d ",max);
         fclose(file);
-    }
-    
 }
 void exportCaculatorResult(FILE *file, char *path,const int first,const int second){
     const char* mode = "w";
     file = fopen(path,mode);
-    if(file == NULL){
-        printf("file không mở được!");
-    }
-    else{
         int summary = add(first,second);
         int different = sub(first,second);
         double product = mul(first,second);
@@ -111,6 +119,4 @@ void exportCaculatorResult(FILE *file, char *path,const int first,const int seco
         fprintf(file,"first: %d, second:%d\n",first,second);
         fprintf(file,"summary: %d, different:%d, product:%.0f, quotient: %.2f",summary,different,product,quotient);
         fclose(file);
-    }
-    
 }
