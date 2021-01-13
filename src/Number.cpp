38646c17c1b38dfa3061a1208c62e78cdde672c1
thaitam <phithaitam@gmail.com>
2021-01-13 12:41:38 +0700

    nhập dữ liệu từ file

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,24 +1,40 @@
 #include<stdio.h>
+void readFile(FILE *file, char *path, int &first, int &second);
 void enter(int *first, int *second);// su dung con tro de nhap
 void enter(int &first, int &second);
 void swap(int *first, int *second);
 void swap(int &first, int &second);// hoán vị sử dụng truyền vào tham chiếu
-int add(const int first, const int second);
-int sub(const int first, const int second);
-double mul(const int first, const int second);
-float div(const int first, const int second);
+int add(const int first, const int second);//TODO: viết hàm cộng 2 số
+int sub(const int first, const int second);//TODO: viết hàm trừ 2 số
+double mul(const int first, const int second);// TODO: viết hàm nhân 2 số
+float div(const int first, const int second);// TODO: viết hàm chia 2 số
 void print(const int first,const int second);// su dung tham tri de xuat, trong khi xuất thì first và second không thay đổi giá trị nên sử dụng thêm từ khóa const trước
 int main(){
+    FILE *file;
+    char* path = "./data/data.in";
     int first, second;
-    enter(&first, &second);// gọi hàm truyền vào con trỏ
+    readFile(file,path,first,second);
+    // enter(&first, &second);// gọi hàm truyền vào con trỏ
     // enter(first,second);// gọi hàm truyền vào tham chiếu
     print(first,second);
     swap(&first, &second);
     printf("\nafter swap: ");
     print(first,second);
-    printf("\nresut after add number:%d",add(first,second));
-    printf("\nresut after sub number:%d",sub(first,second));
+    printf("\nnumber add:%d",add(first,second));
+
     return 0;
+}
+void readFile(FILE *file,char *path, int &first, int &second){
+    const char* mode = "r";
+    file = fopen(path,"r");
+    if(file == NULL){
+        printf("không thể mở file!");
+    }
+    else{
+        fscanf(file,"%d",&first);
+        fscanf(file,"%d",&second);
+    }
+    fclose(file);
 }
 void enter(int *first, int *second){// con tro chua dia chi
     printf("enter first number: ");
@@ -32,8 +48,6 @@ void enter(int &first, int &second){// truyền vào tham chiếu
     printf("enter second number:");
     scanf("%d",&second);
 }
-
-// ddaya la comment
 void swap(int *first, int *second){
     int temp = *first;// *first: lấy giá trị của con trỏ first
     *first = *second;
@@ -46,15 +60,6 @@ void swap(int &first, int &second){
 }
 int add(const int first, const int second){
     return first + second;
-}
-int sub(const int first, const int second){
-    return first - second;
-}
-double mul(const int first, const int second){
-    return first *second;
-}
-float div(const int first, const int second){
-    return (float)first/second;
 }
 void print(const int first,const int second){
     printf("\n{first number:%d, second number: %d}",first,second);
