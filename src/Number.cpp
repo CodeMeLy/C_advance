938c83891480973d0f145ae8d0968d804dd378e1
thaitam <phithaitam@gmail.com>
2021-01-14 23:33:00 +0700

    thêm hàm print in ra 2 số

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void enter(int &first, int &second);// nhập 2 số truyền vào tham chiếu
+void print(int first, int second);
 int main(){
     int first,second;
     enter(first,second);
+    print(first,second);
     return 0;
 }
 void enter(int &first, int &second){
@@ -10,4 +12,7 @@ void enter(int &first, int &second){
     scanf("%d",&first);
     printf("enter second number:");
     scanf("%d",&second);
+}
+void print(int first, int second){
+    printf("{first: %d, second: %d}\n",first,second);
 }
