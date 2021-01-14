23a409e6bf3e668e383d68b9ae33be598f2a8a74
thaitam <phithaitam@gmail.com>
2021-01-14 10:27:44 +0700

    number 3 version

--- src/Number_v2.cpp
+++ src/Number_v2.cpp
@@ -5,16 +5,12 @@ void createDefaultInputFile(FILE *file, char *path,const int first,const int sec
 void import(FILE *file, char *path, int &first, int &second);
 void exportMaxValue(FILE *file, char *path,const int first,const int second);
 void exportCaculatorResult(FILE *file, char *path,const int first,const int second);
-void enter(int *first, int *second);// su dung con tro de nhap
-void enter(int &first, int &second);
 void swap(int *first, int *second);
-void swap(int &first, int &second);// hoán vị sử dụng truyền vào tham chiếu
 int add(const int first, const int second);
 int sub(const int first, const int second);
 double mul(const int first, const int second);
 float div(const int first, const int second);
 int findMax(const int first, const int second);
-void print(const int first,const int second);// su dung tham tri de xuat, trong khi xuất thì first và second không thay đổi giá trị nên sử dụng thêm từ khóa const trước
 int main(){
     FILE *file;
     char* input_path = "./data/data.in";
@@ -24,14 +20,6 @@ int main(){
     import(file,input_path,first,second);
     exportMaxValue(file,max_path,first,second);
     exportCaculatorResult(file,caculator_path,first,second);
-    // enter(&first, &second);// gọi hàm truyền vào con trỏ
-    // enter(first,second);// gọi hàm truyền vào tham chiếu
-    // print(first,second);
-    // swap(&first, &second);
-    // printf("\nafter swap: ");
-    // print(first,second);
-    // printf("\nnumber add:%d",add(first,second));
-
     return 0;
 }
 void createDefaultInputFile(FILE *file, char *path,const int first,const int second){
@@ -55,27 +43,10 @@ void import(FILE *file,char *path, int &first, int &second){
     }
     fclose(file);
 }
-void enter(int *first, int *second){// con tro chua dia chi
-    printf("enter first number: ");
-    scanf("%d",first);// scanf truyen vao 1 dia chi
-    printf("enter second number:");
-    scanf("%d",second);
-}
-void enter(int &first, int &second){// truyền vào tham chiếu
-    printf("enter first number: ");
-    scanf("%d",&first);// scanf truyen vao 1 dia chi, &first: lập địa chỉ của first
-    printf("enter second number:");
-    scanf("%d",&second);
-}
 void swap(int *first, int *second){
     int temp = *first;// *first: lấy giá trị của con trỏ first
     *first = *second;
     *second = temp;
-}
-void swap(int &first, int &second){
-     int temp = first;
-    first = second;
-    second = temp;
 }
 int add(const int first, const int second){
     return first + second;
@@ -91,9 +62,6 @@ float div(const int first, const int second){
 }
 int findMax(const int first, const int second){
     return (first>second)?first:second;
-}
-void print(const int first,const int second){
-    printf("\n{first number:%d, second number: %d}",first,second);
 }
 bool existFile(char*path){
     struct stat buffer;
