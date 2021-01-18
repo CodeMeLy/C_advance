886ec02155f028471f62e0c653ffc536af49b88a
phatnguyen02 <ngphat1211@gmail.com>
2021-01-18 21:22:43 +0700

    soNguyenV2: Hoán vị 2 số sử dụng tham chiếu

--- src/soNguyenV_2.cpp
+++ src/soNguyenV_2.cpp
@@ -1,12 +1,27 @@
 #include <stdio.h>
 void enter(int &first, int &second);//sử dụng tham chiếu
+void output(int first, int second);//xuất 2 số = tham trị
+void swap(int &first, int &second); // hoán vị sử dụng truyền vào tham chiếu
 int main(){
     int first, second;
     enter(first, second);
+    output(first, second);
+    swap(first,second);
+    printf("\nAfter swap");
+    output(first,second);
 }
 void enter(int &first, int &second){
     printf("enter first number: ");
     scanf("%d", &first); // scanf truyền vào 1 địa chỉ
     printf("enter second number: ");
     scanf("%d", &second);
+}
+void output(int first, int second){
+    printf("\nYour number: %d\t%d", first, second);
+}
+void swap(int &first, int &second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
 }
