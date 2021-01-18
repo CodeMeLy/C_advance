1fe09b45e151f7f1f168f39052516b7170f9dfe7
phatnguyen02 <ngphat1211@gmail.com>
2021-01-18 20:41:43 +0700

    soNguyenV1: Hoán vị 2 số sử dụng con trỏ

--- src/soNguyenV_1.cpp
+++ src/soNguyenV_1.cpp
@@ -1,12 +1,26 @@
 #include <stdio.h>
 void enter(int *first, int *second); // con trỏ
+void output(int first, int second); //tham trị
+void swap(int *first, int *second);//hoán vị 2 số = con trỏ
 int main(){
     int first, second;
     enter(&first, &second);
+    output(first, second);
+    swap(&first, &second);
+    printf("\nAfter swap");
+    output(first, second);
 }
 void enter(int *first, int *second){
     printf("enter first number: ");
     scanf("%d", first); // scanf truyền vào 1 địa chỉ
     printf("enter second number: ");
     scanf("%d", second);
+}
+void output(int first, int second){
+    printf("\nYour number: %d\t%d",first,second);
+}
+void swap(int *first, int *second){
+    int temp = *first; // *first: lấy giá trị của con trỏ first
+    *first = *second;
+    *second = temp;
 }
