fb5ea5c363ac01a9251274a0bea5ab80367f91c0
phatnguyen02 <ngphat1211@gmail.com>
2021-01-18 21:27:40 +0700

    soNguyenV2: Tính tổng, hiệu, tích, thương truyền tham trị

--- src/soNguyenV_2.cpp
+++ src/soNguyenV_2.cpp
@@ -2,6 +2,11 @@
 void enter(int &first, int &second);//sử dụng tham chiếu
 void output(int first, int second);//xuất 2 số = tham trị
 void swap(int &first, int &second); // hoán vị sử dụng truyền vào tham chiếu
+int add(const int first, const int second);
+int sub(const int first, const int second);
+double mul(const int first, const int second);
+float div(const int first, const int second);
+void printCaculator(const int first, const int second);
 int main(){
     int first, second;
     enter(first, second);
@@ -9,6 +14,7 @@ int main(){
     swap(first,second);
     printf("\nAfter swap");
     output(first,second);
+    printCaculator(first,second);
 }
 void enter(int &first, int &second){
     printf("enter first number: ");
@@ -24,4 +30,30 @@ void swap(int &first, int &second)
     int temp = first;
     first = second;
     second = temp;
+}
+int add(const int first, const int second)
+{
+    return first + second;
+}
+int sub(const int first, const int second)
+{
+    return first - second;
+}
+double mul(const int first, const int second)
+{
+    return first * second;
+}
+float div(const int first, const int second)
+{
+    return (float)first / second;
+}
+void printCaculator(const int first, const int second)
+{
+    int summary = add(first, second);
+    int different = sub(first, second);
+    double product = mul(first, second);
+    float quotient = div(first, second);
+    output(first, second);
+    printf(" {summary: %d, different:%d, product:%.0f, quotient: %.2f}\n", summary, different, product, quotient);
+    printf("-----------------------------------------");
 }
