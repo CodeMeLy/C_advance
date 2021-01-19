9872cfab03f36987c6bc4edce4f61b1fd5acf40d
thaitam <phithaitam@gmail.com>
2021-01-20 01:17:12 +0700

    Bổ sung hàm in ra mark

--- src/Student_v0.cpp
+++ src/Student_v0.cpp
@@ -6,12 +6,16 @@ typedef struct Mark{
     float chemistry;
 };
 void enter(Mark *input);
+float calculateGPA(Mark *input);
+char* classify(Mark *input);
+void print(Mark *output);
 int main(){
     Mark *mark;// sử dụng biến con trỏ
     mark =(Mark*) malloc(sizeof(Mark));// cấp phát bộ nhớ động cho mark
     enter(mark);
+    print(mark);
     free(mark);
-    
+
     // Mark mark; // sử dụng biến bình thường
     // enter(&mark);
     return 0;
@@ -24,4 +28,29 @@ void enter(Mark *input){
     printf("Enter mark of chemistry: ");
     scanf("%f", &input->chemistry);
 
+}
+float calculateGPA(Mark *input){
+    return (input->math + input->physical + input->chemistry)/3;
+}
+char* classify(Mark *input){
+    float gpa = calculateGPA(input);
+    char* classification = "poor";
+    if(gpa>=8.5){
+        classification = "excellent";
+    }
+    else if(gpa>=7){
+        classification = "good";
+    }
+    else if(gpa>=5){
+        classification = "normal";
+    }
+    else if(gpa>=4){
+        classification = "weak";
+    }
+    return classification;
+}
+void print(Mark *output){
+    float gpa = calculateGPA(output);
+    char *classification = classify(output);
+    printf("Mark:{math: %.2f, physical: %.2f, chemistry:%.2f, gpa: %.2f,classification:%s}\n",output->math,output->physical,output->chemistry,gpa,classification);   
 }
