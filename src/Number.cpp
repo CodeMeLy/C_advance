f1f82fe4e849de770096f4e81c28b1a725a2941d
nguyenhuy <nhuy.a8tqtpy@gmail.com>
2021-01-20 12:22:57 +0700

    thêm hàm sub: trừ 2 số nguyên

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void enter(int &first, int &second);// nhập 2 số truyền vào tham chiếu
 int add(const int first, const int second);// cộng 2 số
+int sub(const int first, const int second);
 void printInputValue(const int first,const int second);// in 2 số truyền vào tham trị
 void printCaculator(const int first, const int second);// in kết quả tính toán
 int main(){
@@ -19,11 +20,15 @@ void enter(int &first, int &second){
 int add(const int first, const int second){
     return first + second;   
 }
+int sub(const int first, const int second){
+    return first - second;
+}
 void printInputValue(const int first,const int second){
     printf("{first: %d, second: %d}\n",first,second);
 }
 void printCaculator(const int first, const int second){
     int sum = add(first, second);
-    printf("{first: %d, second: %d, sum: %d}\n",first,second,sum);
+    int diff = sub(first,second);
+    printf("{first: %d, second: %d, summary: %d, different: %d}\n",first,second,sum,diff);
        
 }
