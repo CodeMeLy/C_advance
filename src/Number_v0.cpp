9c69a5fdf03ab248c7b4455d4524b6d76c42c40d
SmileeeHT <kimsondun@gmail.com>
2021-01-20 21:58:24 +0700

    thêm hàm tính và in ra tổng 2 số

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -1,10 +1,13 @@
 #include<stdio.h>
 void enter(int &first, int &second);
 void print(const int first, const int second);
+int add(const int first, const int second);
+void printCaculator(const int first, const int second);
 int main(){
     int first = 0, second = 0;
     enter(first,second);
     print(first,second);
+    printCaculator(first,second);
     return 0;
 }
 void enter(int &first, int &second){
@@ -14,5 +17,12 @@ void enter(int &first, int &second){
     scanf("%d",&second);
 }
 void print(const int first, const int second){
-    printf("{first:%d, second:%d}",first,second);
+    printf("{first:%d, second:%d}\n",first,second);
+}
+int add(const int first, const int second){
+    return first + second;
+}
+void printCaculator(const int first, const int second){
+    int sum  = add(first,second);
+    printf("{first:%d, second:%d, sum: %d}\n",first,second,sum);
 }
