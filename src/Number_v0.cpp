540ff00d33965b7614c27669f8b9cc4d6dcf76fd
SmileeeHT <kimsondun@gmail.com>
2021-01-20 22:13:08 +0700

    thêm hàm nhân 2 số

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -3,6 +3,8 @@ void enter(int &first, int &second);
 void print(const int first, const int second);
 int add(const int first, const int second);
 int sub(const int first, const int second);
+// tương tự nhân chia: nhân mul, chia div. e viết đi
+int mul(const int first, const int second);
 void printCaculator(const int first, const int second);
 int main(){
     int first = 0, second = 0;
@@ -26,8 +28,12 @@ int add(const int first, const int second){
 int sub(const int first, const int second){
     return first -second;
 }
+int mul(const int first, const int second){
+    return first *second;
+}
 void printCaculator(const int first, const int second){
     int sum  = add(first,second);
     int diff = sub(first,second);
-    printf("{first:%d, second:%d, summary: %d, different: %d}\n",first,second,sum,diff);
+    int product  = mul(first,second);
+    printf("{first:%d, second:%d, summary: %d, different: %d, product: %d}\n",first,second,sum,diff,product);
 }
