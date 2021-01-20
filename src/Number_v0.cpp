0f1c648e3a7ddaaee10d7aa33f852fd981c017c8
ngogiahuy23456 <ngogiahuy23456@gmail.com>
2021-01-20 23:02:20 +0700

    thêm hàm tính tổng 2 số

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void enter(int &first, int &second);
 void print(const int first, const int second);
+int add(const int first, const int second);
 int main(){
     int first = 0,second = 0;
     enter(first,second);// kiểm tra hàm enter đúng hay sai
@@ -13,6 +14,10 @@ void enter(int &first, int &second){
     printf("Enter second number:");
     scanf("%d",&second);
 }
+int add(const int first, const int second){
+    return first+second;
+}
 void print(const int first, const int second){
-    printf("{first:%d, second:%d}",first,second);
+    int sum = add(first,second);
+    printf("{first:%d, second:%d, summary: %d}",first,second,sum);
 }
