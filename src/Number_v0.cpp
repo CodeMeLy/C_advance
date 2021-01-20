4cb845388c263b51b3661f12e8f125a9cbd3f07a
SmileeeHT <kimsondun@gmail.com>
2021-01-20 22:01:10 +0700

    thêm hàm tính và in ra hiệu của 2 số

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -2,6 +2,7 @@
 void enter(int &first, int &second);
 void print(const int first, const int second);
 int add(const int first, const int second);
+int sub(const int first, const int second);
 void printCaculator(const int first, const int second);
 int main(){
     int first = 0, second = 0;
@@ -22,7 +23,11 @@ void print(const int first, const int second){
 int add(const int first, const int second){
     return first + second;
 }
+int sub(const int first, const int second){
+    return first -second;
+}
 void printCaculator(const int first, const int second){
     int sum  = add(first,second);
-    printf("{first:%d, second:%d, sum: %d}\n",first,second,sum);
+    int diff = sub(first,second);
+    printf("{first:%d, second:%d, summary: %d, different: %d}\n",first,second,sum,diff);
 }
