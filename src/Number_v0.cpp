239a6cb5dd5f8ea0efa07378fef79aed702b287a
ngogiahuy23456 <ngogiahuy23456@gmail.com>
2021-01-20 23:23:07 +0700

    thêm hàm nhân 2 số nguyên

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -4,6 +4,7 @@ void print(const int first, const int second);
 int add(const int first, const int second);
 // tương tự hàm cộng em là hàm trừ nhân chia đi. không copy
 int sub(const int first, const int second);
+int mult(const int first, const int second);
 int main(){
     int first = 0,second = 0;
     enter(first,second);// kiểm tra hàm enter đúng hay sai
@@ -22,8 +23,13 @@ int add(const int first, const int second){
 int sub(const int first, const int second){
     return first-second;
 }
+int mul(const int first, const int second){
+    return first*second;
+}
 void print(const int first, const int second){
     int sum = add(first,second);
     int diff = sub(first,second);
-    printf("{first:%d, second:%d, summary: %d, different:%d}",first,second,sum,diff);
+    int mult= mul(first,second);
+
+    printf("{first:%d, second:%d, summary: %d, different:%d, multiplication: %d}",first,second,sum,diff,mult);// share là chia sẻ cảm xúc, chứ k phải chia nhân
 }
