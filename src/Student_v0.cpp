414be20267307e1133db3a1c0c4c0492e7a61471
thaitam <phithaitam@gmail.com>
2021-01-20 13:36:42 +0700

    nhập xuất một ngày

--- src/Student_v0.cpp
+++ src/Student_v0.cpp
@@ -5,17 +5,28 @@ typedef struct Mark{
     float physical;
     float chemistry;
 };
+typedef struct Date{
+    int day;
+    int month;
+    int year;
+};
 void enter(Mark *input);
+void enter(Date *input);
 float calculateGPA(Mark *input);
 char* classify(Mark *input);
 void print(Mark *output);
+void print(Date *output);
 int main(){
     Mark *mark;// sử dụng biến con trỏ
+    Date *date;
     mark =(Mark*) malloc(sizeof(Mark));// cấp phát bộ nhớ động cho mark
+    date = (Date*) malloc(sizeof(Date));
     enter(mark);
+    enter(date);
     print(mark);
+    print(date);
     free(mark);
-
+    free(date);
     // Mark mark; // sử dụng biến bình thường
     // enter(&mark);
     return 0;
@@ -28,6 +39,14 @@ void enter(Mark *input){
     printf("Enter mark of chemistry: ");
     scanf("%f", &input->chemistry);
 
+}
+void enter(Date *input){
+    printf("day: ");
+    scanf("%d",&input->day);
+    printf("month: ");
+    scanf("%d",&input->month);
+    printf("year: ");
+    scanf("%d",&input->year);
 }
 float calculateGPA(Mark *input){
     return (input->math + input->physical + input->chemistry)/3;
@@ -53,4 +72,7 @@ void print(Mark *output){
     float gpa = calculateGPA(output);
     char *classification = classify(output);
     printf("Mark:{math: %.2f, physical: %.2f, chemistry:%.2f, gpa: %.2f,classification:%s}\n",output->math,output->physical,output->chemistry,gpa,classification);   
+}
+void print(Date *output){
+    printf("%d/%d/%d",output->day,output->month,output->year);    
 }
