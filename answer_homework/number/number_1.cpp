cdd7c0eb53215ee06cb5c090895953df852af57a
thaitam <phithaitam@gmail.com>
2021-01-26 16:51:14 +0700

    thêm hàm nhân 2 số

--- answer_homework/number/number_1.cpp
+++ answer_homework/number/number_1.cpp
@@ -18,12 +18,10 @@ int subtract(const int first, const int second){
     return first - second;
 }
 double multiply(const int first, const int second){
-    // TODO: hoàn thiện hàm tính tích 2 số
-
+    return first * second;
 }
 float divide(const int first, const int second){
-    // TODO: hoàn thiện hàm tính thương 2 số
-    return 0;
+    return first * second;
 }
 int findMax(const int first, const int second){
     // TODO: hoàn thiện hàm tìm max của 2 số
