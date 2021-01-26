8cc81d207e67c1eb0031a1b2e17327a26e2b5414
phatnguyen02 <ngphat1211@gmail.com>
2021-01-26 21:59:41 +0700

    number_3: Nhập 2 số và hoán vị dùng Tham chiếu (done)

--- homework/number/number_3.cpp
+++ homework/number/number_3.cpp
@@ -1,35 +1,46 @@
-#include<stdio.h>
-#include<assert.h>
+#include <stdio.h>
+#include <assert.h>
 void enter(int &first, int &second);
-void swap(int &first, int &second);// hoán vị sử dụng truyền vào tham chiếu
+void swap(int &first, int &second); // hoán vị sử dụng truyền vào tham chiếu
 // test function
 void test_enter();
 void test_swap();
-int main(){
-    int first = 0,second = 0;
+int main()
+{
+    int first = 0, second = 0;
     test_enter();
     test_swap();
     return 0;
 }
-void enter(int &first, int &second){
+void enter(int &first, int &second)
+{
     // TODO: hoàn thiện hàm nhập 2 số
+    printf("\nEnter first number: ");
+    scanf("%d", &first);
+    printf("\nEnter second number: ");
+    scanf("%d", &second);
 }
-void swap(int &first, int &second){
+void swap(int &first, int &second)
+{
     // TODO: hoàn thiện hàm hoán vị 2 số
+    int temp = first;
+    first = second;
+    second = temp;
 }
 // test
-void test_enter(){
+void test_enter()
+{
     int first = 0, second = 0;
     printf("nhap first va second(second phai khac 0):");
-    enter(first, second); 
-    assert(second!=0);
+    enter(first, second);
+    assert(second != 0);
     printf("chinh xac!\n");
 }
-void test_swap(){
-    int first = 5,second = 4;
+void test_swap()
+{
+    int first = 5, second = 4;
     printf("hoan vi 2 so:");
     swap(first, second);
-    assert(first== 4 && second== 5);
+    assert(first == 4 && second == 5);
     printf("chinh xac!\n");
 }
-
