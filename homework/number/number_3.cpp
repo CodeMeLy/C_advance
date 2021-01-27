8a58f7244ce710d2544f671566a84962bbfe1706
Dungpi <phamdung.080602@gmail.com>
2021-01-27 18:10:01 +0700

    hoàn thành hàm nhập 2 số

--- homework/number/number_3.cpp
+++ homework/number/number_3.cpp
@@ -12,7 +12,10 @@ int main(){
     return 0;
 }
 void enter(int &first, int &second){
-    // TODO: hoàn thiện hàm nhập 2 số
+    printf("enter number1: ");
+    scanf("%d",&first);
+    printf("enter number2: ");
+    scanf("%d",&second);
 }
 void swap(int &first, int &second){
     // TODO: hoàn thiện hàm hoán vị 2 số
