6ac5a4120d6830a753a4d9cabf3cfdeffdcb9b1e
phatnguyen02 <ngphat1211@gmail.com>
2021-01-28 05:58:24 +0700

    number_4: Nhập và hoán vị 2 số nguyên sử dụng con trỏ

--- homework/number/number_4.cpp
+++ homework/number/number_4.cpp
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// TODO: viết lại number.cpp hoàn chỉnh
-=======
 //TODO: hoàn thiện thành 1 chương trình
 /* Đến hạn 23:59, 1 thg 3
 -  hàm nhập và hoán vị 2 số nguyên sử dụng con trỏ:
@@ -10,4 +7,25 @@
 - export kết quả ra file .out
 - import dữ liệu có sẵn từ một file .in
 - menu */
->>>>>>> homework
+#include <stdio.h>
+void enter(int *first, int *second);
+void swap(int *first, int *second);
+int main(){
+    int first, second;
+    enter(&first,&second);
+    swap(&first, &second);
+}
+void enter(int *first, int *second)
+{   //hàm nhập sử dụng con trỏ
+    printf("\nEnter first number: ");
+    scanf("%d", first);
+    printf("\nEnter second number: ");
+    scanf("%d", second);
+}
+void swap(int *first, int *second)
+{   //hoán vị sử dụng con trỏ
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+    printf("\nAfter swap: %d\t%d", *first, *second);
+}
