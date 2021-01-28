3d68d031824f0a7875e880e77fb923d455fec843
phatnguyen02 <ngphat1211@gmail.com>
2021-01-28 07:14:30 +0700

    number_4: xuất các hàm tính toán ra file

--- homework/number/number_4.cpp
+++ homework/number/number_4.cpp
@@ -22,19 +22,22 @@ void printMax(const int first, const int second);
 void printCaculator(const int first, const int second);
 void exportInputValue(FILE *file, char *path, const int first, const int second);
 void exportMaxValue(FILE *file, char *path, const int first, const int second);
+void exportPrintCaculator(FILE *file, char *path, const int first, const int second);
 int main(){
     int first, second;
     FILE *file;
     char *input_path = "./data/number/inputvalue.out";
     char *max_path = "./data/number/max.out";
+    char *caculator_path = "./data/number/caculator.out";
     enter(&first,&second);
     swap(&first, &second);
     //enter(first,second);
     //swap(first,second);
     //printMax(first,second);
-    printCaculator(first,second);
+    //printCaculator(first,second);
     exportInputValue(file,input_path,first,second);
     exportMaxValue(file,max_path,first,second);
+    exportPrintCaculator(file,caculator_path,first,second);
 }
 void enter(int *first, int *second)
 {   //hàm nhập sử dụng con trỏ
@@ -117,4 +120,15 @@ void exportMaxValue(FILE *file, char *path, const int first, const int second){
     fprintf(file,"max = %d\n",max);
     fprintf(file,"_______________________\n");
     fclose(file);    
+}
+void exportPrintCaculator(FILE *file, char *path, const int first, const int second){
+    char *mode ="a";
+    file= fopen(path,mode);
+    const int summary = add(first, second);
+    const int different = subtract(first, second);
+    const double product = multiply(first, second);
+    const float quotient = divide(first, second);
+    fprintf(file,"inputvalue = {first = %d, second = %d}\n",first,second);//viết vào file có đường dẫn là path
+    fprintf(file,"{summary: %d, different:%d, product:%.0f, quotient: %.2f}", summary, different, product, quotient);
+    fclose(file);
 }
