210fbffb9209b4508623fd1a27b4b44ef73ce34c
phatnguyen02 <ngphat1211@gmail.com>
2021-01-28 07:35:20 +0700

    number_4: xuất Hoán vị ra file

--- homework/number/number_4.cpp
+++ homework/number/number_4.cpp
@@ -21,21 +21,24 @@ int findMax(const int first, const int second);     // tìm max 2 số
 void printMax(const int first, const int second);
 void printCaculator(const int first, const int second);
 void exportInputValue(FILE *file, char *path, const int first, const int second);
+void exportSwap(FILE *file, char *path, int first, int second);
 void exportMaxValue(FILE *file, char *path, const int first, const int second);
 void exportPrintCaculator(FILE *file, char *path, const int first, const int second);
 int main(){
     int first, second;
     FILE *file;
     char *input_path = "./data/number/inputvalue.out";
+    char *swap_path = "./data/number/swap.out";
     char *max_path = "./data/number/max.out";
     char *caculator_path = "./data/number/caculator.out";
     enter(&first,&second);
-    swap(&first, &second);
+    //swap(&first, &second);
     //enter(first,second);
     //swap(first,second);
     //printMax(first,second);
     //printCaculator(first,second);
     exportInputValue(file,input_path,first,second);
+    exportSwap(file,swap_path,first,second);
     exportMaxValue(file,max_path,first,second);
     exportPrintCaculator(file,caculator_path,first,second);
 }
@@ -51,7 +54,7 @@ void swap(int *first, int *second)
     int temp = *first;
     *first = *second;
     *second = temp;
-    printf("\nAfter swap: %d\t%d", *first, *second);
+    //printf("\nAfter swap: %d\t%d", *first, *second);
 }
 void enter(int &first, int &second)
 {   //hàm nhập sử dụng tham chiếu
@@ -65,7 +68,7 @@ void swap(int &first, int &second)
     int temp = first;
     first = second;
     second = temp;
-    printf("\nAfter swap: %d\t%d", first, second);
+    //printf("\nAfter swap: %d\t%d", first, second);
 }
 int add(const int first, const int second)
 {
@@ -110,13 +113,21 @@ void exportInputValue(FILE *file, char *path, const int first, const int second)
     
     fclose(file);
 }
+void exportSwap(FILE *file, char *path, int first, int second){
+    char *mode = "a";
+    file=fopen(path, mode);
+    fprintf(file,"\ninputvalue = {first = %d, second = %d}\n",first,second);
+    swap(&first, &second);
+    fprintf(file,"After swap: %d\t%d", first, second);
+    fclose(file);
+}
 void exportMaxValue(FILE *file, char *path, const int first, const int second){
     char *mode = "a";
     // tìm giá trị max
     const int max = findMax(first,second);
     // xuất ra file
     file = fopen(path,mode);
-    fprintf(file,"inputvalue = {first = %d, second = %d}\n",first,second);//viết vào file có đường dẫn là path
+    fprintf(file,"\ninputvalue = {first = %d, second = %d}\n",first,second);//viết vào file có đường dẫn là path
     fprintf(file,"max = %d\n",max);
     fprintf(file,"_______________________\n");
     fclose(file);    
@@ -128,7 +139,7 @@ void exportPrintCaculator(FILE *file, char *path, const int first, const int sec
     const int different = subtract(first, second);
     const double product = multiply(first, second);
     const float quotient = divide(first, second);
-    fprintf(file,"inputvalue = {first = %d, second = %d}\n",first,second);//viết vào file có đường dẫn là path
+    fprintf(file,"\ninputvalue = {first = %d, second = %d}\n",first,second);//viết vào file có đường dẫn là path
     fprintf(file,"{summary: %d, different:%d, product:%.0f, quotient: %.2f}", summary, different, product, quotient);
     fclose(file);
 }
