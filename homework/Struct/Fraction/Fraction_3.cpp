fc12ba0b6cae253e6b2efecd23b35e684ab87245
thaitam <phithaitam@gmail.com>
2021-01-28 12:57:33 +0700

    sửa fraction_3

--- homework/Struct/Fraction/Fraction_3.cpp
+++ homework/Struct/Fraction/Fraction_3.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h>// thư viện để cấp phát
 #include<math.h>
 #include<string.h>
 #include<assert.h>
@@ -11,6 +11,8 @@ void enter(Fraction *input);// nhập phân số
 void test_enter();
 int main(){
     Fraction *fraction;
+    // vì biến fraction là con trỏ nên cần phải cấp phát bộ nhớ
+    fraction = (Fraction*)malloc(sizeof(Fraction));// cấp phát bộ nhớ
     // TODO: viết hàm enter
     enter(fraction);
     return 0;
