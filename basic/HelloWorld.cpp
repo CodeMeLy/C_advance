7567960e7817029b073ea5f37f2aa41eb0d82b7a
redduck-02 <76261980+redduck-02@users.noreply.github.com>
2021-01-28 16:05:08 +0700

    thêm helloworld!

---
+++ basic/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Hello world!");
+    return 0;
+}
+// ctrl + để thu to, ctrl - để zoom nhỏ lại. Cái này khi nào mình code ý. nên zoom to lên, 
+// còn khi e đọc code thì nên zoom nhỏ lại. cái này là tips ý
+// đơn giản vậy nhưng nó cho hiệu quả cao. ví dụ nhé. anh zô 1 file code dài hơn
