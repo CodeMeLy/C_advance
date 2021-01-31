480468f4e47b9d1ac03b650e2f506b1a77f3a4fb
phatnguyen02 <ngphat1211@gmail.com>
2021-01-31 18:14:38 +0700

    homework/Money_1: update

--- homework/money/Money_1.cpp
+++ homework/money/Money_1.cpp
@@ -1,13 +1,50 @@
-/* TODO: 
-1.viết hàm nhập vào USD và tìm ra VND
-2.viết hàm nhập vào VND và tìm ra USD */
+#include<stdio.h>
 void enterMoney(float *input);// nhập số tiền
 void enterExchangeRate(float *input);// nhập tỉ giá
-float transfer(const float money, const float exchange_rage);// chuyển đổi tiền
-void printTransferValue(const float money, const float exchange_rage);// in ra giá trị sau khi chuyển đổi
-void menu(const float money, const float exchange_rage);// hàm menu
+float transfer(const float money, const float exchange_rate);// chuyển đổi tiền
+void printTransferValue(const float money, const float exchange_rate);// in ra giá trị sau khi chuyển đổi
+void menu(const float money, const float exchange_rate);// hàm menu
 int main(){
-    float money,exchange_rage;
+    float money,exchange_rate;
+    enterMoney(&money);
+    enterExchangeRate(&exchange_rate);
+    menu(money,exchange_rate);
     return 0;
 }
-// TODO: hoàn thiện các hàm trên
+void enterMoney(float *input){
+    printf("Enter amount of money you want to convert: ");
+    scanf("%f",input);
+}
+void enterExchangeRate(float *input){
+    printf("Enter exchange rate: ");
+    scanf("%f",input);
+}
+float transfer(const float money, const float exchange_rate){
+    return money*exchange_rate;
+}
+void printTransferValue(const float money, const float exchange_rate){
+    printf("kết quả: %.2f",transfer(money, exchange_rate));
+}
+void menu(const float money, const float exchange_rate){
+    int choise;
+    do{
+        printf("\n1. Convert VND to USD");
+        printf("\n2. Convert USD to VND");
+        printf("\n----------------------");
+        printf("\nYour choise: ");
+        scanf("%d", &choise);
+        switch (choise)
+        {
+        case 1:
+            printf("\nYour money: %.3f $",transfer(money,exchange_rate));
+            break;
+        case 2:
+            printf("\nYour money: %.3f VND",transfer(money,exchange_rate));
+            break;
+        default:
+           printf("\nChoose again!");
+            break;
+        }
+        printf("\nDo you want to exit? (press 0)");
+    } while (choise);
+}
