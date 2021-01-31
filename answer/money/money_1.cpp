8bbf443328da323974cce62dc7c6ab5c6bbca90d
phatnguyen02 <ngphat1211@gmail.com>
2021-01-31 18:05:16 +0700

    answer/money_1: thêm hàm menu (done)

--- answer/money/money_1.cpp
+++ answer/money/money_1.cpp
@@ -1,6 +1,3 @@
-/* TODO: 
-1.viết hàm nhập vào USD và tìm ra VND
-2.viết hàm nhập vào VND và tìm ra USD */
 #include<stdio.h>
 void enterMoney(float *input);// nhập số tiền
 void enterExchangeRate(float *input);// nhập tỉ giá
@@ -11,16 +8,15 @@ int main(){
     float money,exchange_rate;
     enterMoney(&money);
     enterExchangeRate(&exchange_rate);
-    printTransferValue(money, exchange_rate);
+    menu(money,exchange_rate);
     return 0;
 }
-// TODO: hoàn thiện các hàm trên
 void enterMoney(float *input){
-    printf("số tiền muốn chuyển:");
+    printf("Enter amount of money you want to convert: ");
     scanf("%f",input);
 }
 void enterExchangeRate(float *input){
-    printf("tỉ giá:");
+    printf("Enter exchange rate: ");
     scanf("%f",input);
 }
 float transfer(const float money, const float exchange_rate){
@@ -28,4 +24,27 @@ float transfer(const float money, const float exchange_rate){
 }
 void printTransferValue(const float money, const float exchange_rate){
     printf("kết quả: %.2f",transfer(money, exchange_rate));
+}
+void menu(const float money, const float exchange_rate){
+    int choise;
+    do{
+        printf("\n1. Convert VND to USD");
+        printf("\n2. Convert USD to VND");
+        printf("\n----------------------");
+        printf("\nYour choise: ");
+        scanf("%d", &choise);
+        switch (choise)
+        {
+        case 1:
+            printf("\nYour money: %.3f $",transfer(money,exchange_rate));
+            break;
+        case 2:
+            printf("\nYour money: %.3f VND",transfer(money,exchange_rate));
+            break;
+        default:
+           printf("\nChoose again!");
+            break;
+        }
+        printf("\nDo you want to exit? (press 0)");
+    } while (choise);
 }
