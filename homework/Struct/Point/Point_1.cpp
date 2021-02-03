91b176799bd355522a81c0ede98573810e975a14
thaitam <phithaitam@gmail.com>
2021-02-03 23:04:00 +0700

    sửa bài điểm

--- homework/Struct/Point/Point_1.cpp
+++ homework/Struct/Point/Point_1.cpp
@@ -13,8 +13,8 @@ typedef struct Point{
 void enter(Point *input);// nhập vào 1 điểm
 void enter(Point *first, Point *second, Point *third);// nhập vào 3 đỉnh của tam giác
 double findDistance(const Point first, const Point second);// tính khoảng cách 2 điểm
-void findTrianglePerimeter(const Point first, const Point second, const Point third);// tính chu vi tam giác
-void findTriangleArea(const Point first, const Point second, const Point third);// tính diện tích tam giác
+double findTrianglePerimeter(const Point first, const Point second, const Point third);// tính chu vi tam giác
+double findTriangleArea(const Point first, const Point second, const Point third);// tính diện tích tam giác
 void canFormEgdeOfTriAngle(const Point first, const Point second);// 2 điểm có thể tạo thành một cạnh tam giác?
 void isPointOfTriAngle(const Point first, const Point second, const Point third);// là 3 đỉnh của tam giác?
 bool isEquilateralTriangle(const Point first, const Point second, const Point third);// là tam giác đều
@@ -28,8 +28,8 @@ void printTriangleArea(const Point first, const Point second, const Point third)
 int main(){
     Point first, second, third;
     enter(&first,&second,&third);
-    findTrianglePerimeter(first,second,third);
-    findTriangleArea(first,second,third);
+    printTrianglePerimeter(first, second, third);
+    printTriangleArea(first,second,third);
     return 0;
 }
 // TODO: hoàn thiện các hàm
@@ -48,17 +48,29 @@ void enter(Point *first, Point *second, Point *third){
     enter(third);
 }
 double findDistance(const Point first, const Point second){
-    double distance;
-    distance = sqrt((double)(first.x-second.x)*(first.x-second.x)+(first.y-second.y)*(first.y-second.y));
+    double distance = sqrt((double)(first.x-second.x)*(first.x-second.x)+(first.y-second.y)*(first.y-second.y));//NOTE: khởi gán luôn nhé
     return distance;
 }
-void findTrianglePerimeter(const Point first, const Point second, const Point third){
-    double AB=findDistance(first,second), AC=findDistance(first,third), BC=findDistance(second,third);
-    printf("\nAB= %.2lf\tAC= %.2lf\tBC= %.2lf",AB,AC,BC);
-    printf("\nPerimeter of triangle is: %.2lf",AB+AC+BC);
+double findTrianglePerimeter(const Point first, const Point second, const Point third){// NOTE: có những chỗ a có thể sai nên sửa lại nhé
+    // NOTE: đưa xuống cho dễ đọc nhé
+    double AB = findDistance(first,second);
+    double AC = findDistance(first,third);
+    double BC = findDistance(second,third);
+    return AB + AC + BC;
 }
-void findTriangleArea(const Point first, const Point second, const Point third){
-    double p=(findDistance(first,second)+findDistance(first,third)+findDistance(second,third))/2.0;
-    double S=sqrt((double)p*(p-findDistance(first,second))*(p-findDistance(first,third))*(p-findDistance(second,third)));
-    printf("\nArea of triagle is: %.2lf", S);
+double findTriangleArea(const Point first, const Point second, const Point third){
+    // sửa lại nhé
+    double AB = findDistance(first,second);
+    double AC = findDistance(first,third);
+    double BC = findDistance(second,third);
+    double triagle_perimeter = findTrianglePerimeter(first,second,third);
+    return sqrt(triagle_perimeter*(triagle_perimeter-AB)*(triagle_perimeter-AC)*(triagle_perimeter-BC));
+}
+void printTrianglePerimeter(const Point first, const Point second, const Point third){
+    double triagle_perimeter = findTrianglePerimeter(first,second,third);
+    printf("triangle perimeter = %lf\n",triagle_perimeter);
+}
+void printTriangleArea(const Point first, const Point second, const Point third){
+    double triagle_area = findTriangleArea(first,second,third);
+    printf("triangle area = %lf\n",triagle_area);
 }
