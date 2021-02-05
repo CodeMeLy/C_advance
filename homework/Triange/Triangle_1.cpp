fba5bcd4cd9c626c16379663ba0e97ac5b2bc159
phatnguyen02 <ngphat1211@gmail.com>
2021-02-05 10:29:42 +0700

    Triangle_1: nhập các hàm kiểm tra loại tam giác

--- homework/Triange/Triangle_1.cpp
+++ homework/Triange/Triangle_1.cpp
@@ -6,6 +6,7 @@
 - TH2: nếu nó không lập thành tam giác, in ra màn hình không lập thành tam giác
  */
 #include<stdio.h>
+#include<math.h>
 #include<assert.h>
 bool isValid(const float egde);// kiểm tra một cạnh hợp lệ hay không?
 void enter(float *first, float *second, float *third);// nhập 3 cạnh tam giác
@@ -46,4 +47,16 @@ void enter(float *first, float *second, float *third){
 }
 bool isEgdeOfTriAngle(const float first, const float second, const float third){
     return first+second>third && first+third>second && second+third>first;
+}
+bool isEquilateralTriangle(const float first, const float second, const float third){
+    return first==second&&first==third&&second==third;
+}
+bool isIsoscelesTriangle(const float first, const float second, const float third){
+    return first==second||first==third||second==third;
+}
+bool isRightTriangle(const float first, const float second, const float third){
+    return pow(first,2)==pow(second,2)+pow(third,2)||pow(second,2)==pow(first,2)+pow(third,2)||pow(third,2)==pow(first,2)+pow(second,2);
+}
+bool isRightIsoscelesTriangle(const float first, const float second, const float third){
+    return isRightIsoscelesTriangle(first,second,third)&&isRightTriangle(first,second,third);
 }
