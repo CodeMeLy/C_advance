564c5d9106fd0a35231e375c098145b0ad4fc034
phatnguyen02 <ngphat1211@gmail.com>
2021-02-06 17:46:23 +0700

    Money_2: In ra cách mà tổng số lượng tờ tiền ít nhất

--- homework/money/Money_2.cpp
+++ homework/money/Money_2.cpp
@@ -6,13 +6,15 @@
 #include<stdio.h>
 bool isValid(const double money);// kiểm tra số tiền nhập vào hợp lệ
 void enter(double *money);// nhập số tiền
-void countNumbersOfWaysToDivide(double money);// tìm số cách chia
+void printNumbersOfWaysToDivide(double money);// in ra số cách chia
 void digitsWaysToDivide(double money);// liệt kê các cách chia tiền
+void printOptimalWayToDivide(double money);//in ra cách mà tổng số lượng tờ tiền ít nhất
 int main(){
     double money;
     enter(&money);
-    countNumbersOfWaysToDivide(money);
+    printNumbersOfWaysToDivide(money);
     digitsWaysToDivide(money);
+    printOptimalWayToDivide(money);
     return 0;
 }
 // TODO: hoàn thiện các hàm
@@ -28,7 +30,7 @@ void enter(double *money){
         }
     }while(!isValid(*money));
 }
-void countNumbersOfWaysToDivide(double money){
+void printNumbersOfWaysToDivide(double money){
     int count=0;
     int fiveK, twoK, oneK;
     for(fiveK=0; fiveK<=money/5000; fiveK++){
@@ -50,5 +52,16 @@ void digitsWaysToDivide(double money){
                 printf("\n%d type 5000, %d type 2000, %d type 1000",fiveK,twoK,oneK);
             }
         }
+    }
+}
+void printOptimalWayToDivide(double money){
+    int count;
+    int moneyChange[3]={5000, 2000, 1000};
+    for(int i=0;i<3;i++){
+        count=(int)money/moneyChange[i];
+        if(count !=0){
+            printf("\n%d type of money %d",count,moneyChange[i]);
+        }
+        money= (int)money % moneyChange[i];
     }
 }
