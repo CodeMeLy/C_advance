8c9ac0439ac8f06899cf3657e25a93e26cbe9358
phatnguyen02 <ngphat1211@gmail.com>
2021-02-06 21:53:41 +0700

    :P

--- src/mangConTro.cpp
+++ src/mangConTro.cpp
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define Max 100
 void enter(int &size);
 void enter(int **array, int size);
+void output(int **array, int size);
 int main(){
     int **array, size;
     array=(int**)(malloc(size*sizeof(int*)));
     enter(size);
     enter(array, size);
+    output(array,size);
 }
 void enter(int &size){
     printf("Enter n: ");
@@ -15,6 +18,11 @@ void enter(int &size){
 void enter(int **array, int size){
     for(int i=0;i<size;i++){
         printf("a[%d]: ",i+1);
-        scanf("%d", &array);
+        scanf("%d", (array+i));
+    }
+}
+void output(int **array, int size){
+    for(int i=0;i<size;i++){
+        printf("\t%d", **(array+i));
     }
 }
