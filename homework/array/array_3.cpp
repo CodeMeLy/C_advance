54828fe4dec6124f51ad752643c5337f28e63d58
phatnguyen02 <ngphat1211@gmail.com>
2021-02-09 17:50:09 +0700

    Array_3: tìm vị trí của giá trị nhập vào

--- homework/array/array_3.cpp
+++ homework/array/array_3.cpp
@@ -15,16 +15,21 @@ int findMax(int *array, int size);// tìm giá trị max
 void printMax(int *array, int size);
 int findMin(int *array, int size);// tìm giá trị min
 void printMin(int *array, int size);
+void enterValue(int &value);
+int countNumbersOfValue(int *array, int size, int value); // đếm số lần xuất hiện của value
 int* findIndexOfValue(int *array, int size, int value);// tìm vị trí của một giá trị nhập vào
+void printLocationOfValue(int *array, int size, int value);
 int* findIndexOfMin(int *array, int size);// tìm vị trí min
 int* findIndexOfMax(int *array, int size);// tìm vị trí max
 int main(){
-    int *array, size;
+    int *array, size, value;
     enter(size);
     array = (int*)calloc(size, sizeof(int));
     enter(array,size);
     printMax(array,size);
     printMin(array,size);
+    enterValue(value);
+    printLocationOfValue(array,size,value);
     return 0;    
 }
 void enter(int &size){
@@ -60,4 +65,34 @@ int findMin(int *array, int size){
 }
 void printMin(int *array, int size){
     printf("\nMin value of array is: %d", findMin(array,size));
+}
+void enterValue(int &value){
+    printf("\nEnter a value: ");
+    scanf("%d", &value);
+}
+int countNumbersOfValue(int *array, int size, int value){
+    int count = 0;
+    for(int index = 0; index < size; index++){
+        if(array[index] == value){
+            count++;
+        }
+    }
+    return count;
+}
+int* findIndexOfValue(int *array, int size, int value){
+    int *result;
+    int temp = 0;
+    int size_result = countNumbersOfValue(array, size,value);
+    result = (int*)calloc(size_result,sizeof(int));// cấp phát bộ nhớ
+    for(int index = 0; index < size; index++){
+        if(value == array[index]){
+            result[temp] =  index;
+            temp++;
+        }
+    }
+    return result;
+}
+void printLocationOfValue(int *array, int size, int value){
+    int *result=findIndexOfValue(array,size,value);
+    printf("Location of value is: %d", *result);
 }
