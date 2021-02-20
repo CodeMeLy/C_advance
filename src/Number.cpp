83fa761db34a47bf8484827dcd5a4bda52108ae1
votrong <votrong15112017@gmail.com>
2021-02-20 19:33:43 +0700

    them ham tinh tong 2 so

--- src/Number.cpp
+++ src/Number.cpp
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 void enter(int &first, int &second);
+int add(int first, int second);
 void print(int first, int second);
+void printCaculator(int first, int second);
 int main(){
     int first,second;
     enter(first,second);
     print(first,second);
+    printCaculator(first,second);
     return EXIT_SUCCESS;
 }
 void enter(int &first, int &second){
@@ -14,6 +17,12 @@ void enter(int &first, int &second){
     printf("Enter second number:");
     scanf("%d",&second);
 }
+int add(int first, int second){
+    return first + second;
+}
 void print(int first, int second){
-    printf("{first: %d, second: %d}",first,second);
+    printf("- first: %d, second: %d\n",first,second);
+}
+void printCaculator(int first, int second){
+    printf("- %d + %d = %d\n",first,second,add(first,second));
 }
