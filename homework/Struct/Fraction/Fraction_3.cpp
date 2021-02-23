3ee70532b306ac6b3d1f8a2dd0638472389fa653
hoangvy201002 <vychuho.201002@gmail.com>
2021-02-23 16:26:56 +0700

    hoàn thành fraction 3

--- homework/Struct/Fraction/Fraction_3.cpp
+++ homework/Struct/Fraction/Fraction_3.cpp
@@ -11,9 +11,18 @@ void enter(Fraction *input);// nhập phân số
 void test_enter();
 int main(){
     Fraction *fraction;
-    // TODO: viết hàm enter
+    fraction = (Fraction*)malloc(sizeof(Fraction));
+    enter(fraction);
     return 0;
 }
 void enter(Fraction *input){
-    // TODO: hoàn thiện hàm nhập một phân số
+    printf("enter numerator:");
+    scanf("%d", &input -> numerator);
+    do{
+        printf("enter denominator:");
+        scanf("%d", &input -> denominator);
+        if(input -> denominator == 0){
+            printf("review:");
+        }
+    }while(input -> denominator == 0);
 }
